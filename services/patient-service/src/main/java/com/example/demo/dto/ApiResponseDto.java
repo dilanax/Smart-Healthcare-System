9cf537b5054d354c0333abc3d7df6edda6a44d80
pathumthiw <pathumthivanka12asd@gmail.com>
2026-04-08 00:34:47 +0530

    Update ApiResponseDto.java

--- services/patient-service/src/main/java/com/example/demo/dto/ApiResponseDto.java
+++ services/patient-service/src/main/java/com/example/demo/dto/ApiResponseDto.java
@@ -1,11 +1,30 @@
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class ApiResponseDto {
     private String message;
     private Object data;
+
+    public ApiResponseDto() {
+    }
+
+    public ApiResponseDto(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
